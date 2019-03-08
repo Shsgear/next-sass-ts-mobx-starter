834cd694910f7701a39b9feb87b70ace69130f75
孙皓 <shsgear@qq.com>
2019-03-08 19:32:34 +0800

    add arrow function in class component

--- components/Hello.tsx
+++ components/Hello.tsx
@@ -4,6 +4,7 @@ interface Props {
   name: string,
   enthusiasmLevel?: number
 }
+
 // stateless function component
 const Hello : React.FunctionComponent<Props> = ({ name, enthusiasmLevel = 1 }: Props) : JSX.Element => {
   if (enthusiasmLevel <= 0) {
@@ -19,30 +20,45 @@ const Hello : React.FunctionComponent<Props> = ({ name, enthusiasmLevel = 1 }: P
 }
 
 interface IAgeObject {
-  age: number;
+  currentEnthusiasm: number;
 }
 // class component
 class Hello1 extends React.Component<Props, IAgeObject> {
   constructor(props:Props) {
     super(props);
     this.state = {
-      age: 11,
+      currentEnthusiasm: props.enthusiasmLevel || 1,
     }
   }
-  render() {
-    const { name, enthusiasmLevel = 1 } = this.props;
 
-    if (enthusiasmLevel <=0) {
+
+  onIncrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm + 1);
+  onDecrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm - 1);
+  
+  
+  render() {
+    const { name } = this.props;
+    
+
+    if (this.state.currentEnthusiasm <= 0) {
       throw new Error('more enthusiastic :D');
     }
 
     return (
       <div className='hello'>
         <div className='greeting'>
-          Hello { name + getExclamationMarks(enthusiasmLevel) } you are { this.state.age }
+          Hello { name + getExclamationMarks(this.state.currentEnthusiasm) }
+        </div>
+        <div>
+          <button onClick={this.onDecrement}>-</button>
+          <button onClick={this.onIncrement}>+</button>
         </div>
       </div>
     )
+  }
+
+  updateEnthusiasm(currentEnthusiasm: number) {
+    this.setState({ currentEnthusiasm })
   }
   
 }
